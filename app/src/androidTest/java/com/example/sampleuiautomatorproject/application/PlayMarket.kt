9ced173ac4a2abbe59574d0ef6e3b36573d3bc32
Silencegod <gosha-95@bk.ru>
2019-11-11 02:59:13 +0300

    Moved actions to page class

--- app/src/androidTest/java/com/example/sampleuiautomatorproject/application/PlayMarket.kt
+++ app/src/androidTest/java/com/example/sampleuiautomatorproject/application/PlayMarket.kt
@@ -1,27 +1,16 @@
-package com.example.sampleuiautomatorproject
+package com.example.sampleuiautomatorproject.application
 
 import android.content.Intent
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.sampleuiautomatorproject.util.byStringRes
 import com.example.sampleuiautomatorproject.util.byText
 import com.example.sampleuiautomatorproject.util.context
 import com.example.sampleuiautomatorproject.util.device
 import com.example.sampleuiautomatorproject.util.ext.*
 import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class InstallHabrTest {
+class PlayMarket {
 
-    @Before
-    fun setUp() {
+    fun open() {
         val intent =
             context.packageManager.getLaunchIntentForPackage("com.android.vending")?.apply {
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -30,24 +19,40 @@ class InstallHabrTest {
         context.startActivity(intent)
     }
 
-    @Test
-    fun installHabr() {
+    fun clickSearch() {
         byStringRes("com.android.vending:id/search_bar_hint")
             .waitFindObject()
             .click()
+    }
+
+    fun typeToSearch(text: String) {
         byStringRes("com.android.vending:id/search_bar_text_input")
             .waitFindObject()
-            .text = "Habrahabr"
-        byText("Habr — сообщество IT-специалистов")
+            .text = text
+    }
+
+    fun clickResult(resultName: String) {
+        byText(resultName)
             .waitFindObject()
             .click()
+    }
+
+    fun clickInstall() {
         byText("Установить")
             .waitFindObject()
             .click()
-        val isInstalled = byText("Открыть")
-            .waitFindObject()
-            .waitIsClickable(true, 20.secondsToMillis())
-        assertTrue("Приложение не установилось", isInstalled)
+    }
+
+    fun waitForInstalled() {
+        assertTrue(
+            "Приложение не было установлено",
+            byText("Открыть")
+                .waitFindObject()
+                .waitIsClickable(true, 20.secondsToMillis())
+        )
+    }
+
+    fun openInstalledApp() {
         byText("Открыть")
             .findObject()
             .clickAndWaitnewWindow()
---
+++ app/src/androidTest/java/com/example/sampleuiautomatorproject/test/InstallHabrTest.kt
@@ -0,0 +1,26 @@
+package com.example.sampleuiautomatorproject.test
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.sampleuiautomatorproject.application.PlayMarket
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class InstallHabrTest {
+
+    @Test
+    fun installHabr() = with(PlayMarket()) {
+        open()
+        clickSearch()
+        typeToSearch("Habrahabr")
+        clickResult("Habr — сообщество IT-специалистов")
+        clickInstall()
+        waitForInstalled()
+        openInstalledApp()
+    }
+}
